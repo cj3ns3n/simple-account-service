03f4544d2e533c5410859e329b26c4a17c618eb5
Jensen <cjensen@am.sony.com>
2017-01-28 10:19:34 -0800

    Import cleanup for tests

--- src/test/java/org/jensens/service/account/AccountControllerManagementTest.java
+++ src/test/java/org/jensens/service/account/AccountControllerManagementTest.java
@@ -1,12 +1,5 @@
 package org.jensens.service.account;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.jensens.service.account.storage.Account;
 import org.junit.Assert;
 import org.junit.Test;
@@ -16,9 +9,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-
-import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
