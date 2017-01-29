5e01423a83cc74189e3aac53d311761052272ad7
Jensen <cjensen@am.sony.com>
2017-01-29 10:30:27 -0800

    Elite iterations

--- src/main/java/org/jensens/service/account/CustomErrorController.java
+++ src/main/java/org/jensens/service/account/CustomErrorController.java
@@ -3,14 +3,12 @@ package org.jensens.service.account;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.web.ErrorController;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import org.apache.commons.io.IOUtils;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
